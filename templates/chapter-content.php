9d47334ea499ad7229825c9405605b210179dd63
Coleman Haley <coleman.c.haley@gmail.com>
2017-08-02 14:34:58 -0500

    Update templates--tables.

--- templates/chapter-content.php
+++ templates/chapter-content.php
@@ -9,14 +9,59 @@
  */
 $university_id = get_post_meta(get_the_ID(), 'university', true);
 ?>
+	<style>
+		/* DivTable.com */
+		.divTable{
+			display: table;
+			width: 100%;
+		}
+		.divTableRow {
+			display: table-row;
+		}
+		.divTableHeading {
+			background-color: #EEE;
+			display: table-header-group;
+		}
+		.divTableCell, .divTableHead {
+			border: 1px solid #999999;
+			display: table-cell;
+			padding: 3px 10px;
+		}
+		.divTableHeading {
+			background-color: #EEE;
+			display: table-header-group;
+			font-weight: bold;
+		}
+		.divTableFoot {
+			background-color: #EEE;
+			display: table-footer-group;
+			font-weight: bold;
+		}
+		.divTableBody {
+			display: table-row-group;
+		}
+	</style>
 	<article id="post-<?php the_ID(); ?>" <?php post_class( 'tm-box-content' ); ?>>
 		<div class="entry-header">
 				<div class="post-categories">Chapter</div>
 				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 		</div>
 		<div class="entry-content">
+			<div class="divTable">
+				<div class="divTableBody">
+					<div class="divTableRow">
+						<div class="divTableCell">&nbsp;
+							<h2>Chapter History:</h2>
+							<?php echo get_user_meta($university_id, 'chapter_history', true); ?>
+						</div>
+						<div class="divTableCell">&nbsp;
+							<h3>Chapter Bylaws:</h3>
+							<?php echo Insight::setting('header_type'); ?>
+						</div>
+					</div>
+				</div>
+			</div>
 			<?php
-			echo get_user_meta($university_id, 'chapter_history', true);
 			the_content( sprintf( /* translators: %s: Name of current post. */
 				             wp_kses( __( 'Continue reading %s <span class="meta-nav">&rarr;</span>', 'tm-arden' ), array( 'span' => array( 'class' => array() ) ) ), the_title( '<span class="screen-reader-text">"', '"</span>', false ) ) );
 
