270f538dc73cba88824a7cc3892fad107e5225e7
Coleman Haley <coleman.c.haley@gmail.com>
2017-08-02 17:11:23 -0500

    Restore metaboxes.

--- admin/class-opsi-util-chapters.php
+++ admin/class-opsi-util-chapters.php
@@ -154,7 +154,6 @@ class Opsi_Util_Chapters {
 
 	public function register_metabox() {
 		  $prefix = 'opsi_chapter_';
-		  wp_die("k");
 		  /**
 		   * Sample metabox to demonstrate each field type included
 		   */
