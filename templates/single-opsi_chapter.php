562b47a698dc666abb208222dbc8e5d32a9bc92d
Coleman Haley <coleman.c.haley@gmail.com>
2017-08-02 14:13:10 -0500

    Update chapter template.

--- templates/single-opsi_chapter.php
+++ templates/single-opsi_chapter.php
@@ -9,28 +9,11 @@
  */
 get_header();
 
-$page_sidebar_position = Insight_Helper::get_post_meta( 'page_sidebar_position', 'default' );
-$page_sidebar1         = Insight_Helper::get_post_meta( 'page_sidebar_1', 'default' );
-$page_sidebar2         = Insight_Helper::get_post_meta( 'page_sidebar_2', 'default' );
-
-if ( $page_sidebar1 === 'default' ) {
-	$page_sidebar1 = Insight::setting( 'post_page_sidebar_1' );
-}
-
-if ( $page_sidebar2 === 'default' ) {
-	$page_sidebar2 = Insight::setting( 'post_page_sidebar_2' );
-}
-
-if ( $page_sidebar_position === 'default' ) {
-	$page_sidebar_position = Insight::setting( 'post_page_sidebar_position' );
-}
 ?>
 <?php get_template_part( 'components/title-bar' ); ?>
 	<div id="page-content" class="page-content">
 		<div class="container">
 			<div class="row">
-
-				<?php Insight_Templates::render_sidebar( $page_sidebar_position, $page_sidebar1, $page_sidebar2, 'left' ); ?>
 
 				<div class="page-main-content col-md-8">
 					<?php
@@ -54,8 +37,6 @@ if ( $page_sidebar_position === 'default' ) {
 					endwhile; // End of the loop.
 					?>
 				</div>
-
-				<?php Insight_Templates::render_sidebar( $page_sidebar_position, $page_sidebar1, $page_sidebar2, 'right' ); ?>
 
 			</div>
 		</div>
